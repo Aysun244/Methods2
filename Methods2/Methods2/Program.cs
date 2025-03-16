130fca6454787cf605d88930737ff1fe9a6c7c87
Aysun Naghizada <aysunin-bb104@code.edu.az>
2025-03-16 23:30:45 +0400

    MinElement1

--- Methods2/Methods2/Program.cs
+++ Methods2/Methods2/Program.cs
@@ -1,25 +1,25 @@
-﻿//using System;
-//class Program
+﻿using System;
+class Program
 
-//{
-//    static void Min()
-//    {
-//        int[] arr = { 5, 43, 67, 21, 2, 45, 667, 3234 };
-//        int min = arr[0];
-//        for (int i = 1; i < arr.Length; i++)
-//        {
-//            if (arr[i] < min)
-//                min = arr[i];
-//        }
+{
+    static void Min()
+    {
+        int[] arr = { 5, 43, 67, 21, 2, 45, 667, 3234 };
+        int min = arr[0];
+        for (int i = 1; i < arr.Length; i++)
+        {
+            if (arr[i] < min)
+                min = arr[i];
+        }
 
-//        Console.WriteLine("En kicik eded:" + min);
-//    }
-//    static void Main()
-//            {
-//                Min();
-//            }
+        Console.WriteLine("En kicik eded:" + min);
+    }
+    static void Main()
+    {
+        Min();
+    }
 
-//}
+}
 
 
 //using System;
@@ -63,46 +63,46 @@
 //    }
 //}
 
-using System;
-class Program
+//using System;
+//class Program
 
-{
-    //Cevrenin S-i
-    static double Area(double r)
-    {
-        double p = 3;
-        return p * r * r;
-    }
+//{
+//    //Cevrenin S-i
+//    static double Area(double r)
+//    {
+//        double p = 3;
+//        return p * r * r;
+//    }
 
-    //Duzbucaqlinin S-i
-    static double Area(double a, double b)
-    {
-        return a * b;
-    }
+//    //Duzbucaqlinin S-i
+//    static double Area(double a, double b)
+//    {
+//        return a * b;
+//    }
 
-    //Duzbucaqli paralelpipedin tam sethinin S-i
-    static double Area(double a, double b, double c)
-    {
-        return 2 * (a * b + a * c + b * c);
-    }
+//    //Duzbucaqli paralelpipedin tam sethinin S-i
+//    static double Area(double a, double b, double c)
+//    {
+//        return 2 * (a * b + a * c + b * c);
+//    }
 
-    //3bucagin daxilie cekilmis cevrenin S-i
-    static double Area(double a, double b, double c, double r)
-    {
+//    //3bucagin daxilie cekilmis cevrenin S-i
+//    static double Area(double a, double b, double c, double r)
+//    {
 
-        double p = (a + b + c) / 2;
-        return p * r;
-    }
-    static void Main()
+//        double p = (a + b + c) / 2;
+//        return p * r;
+//    }
+//    static void Main()
 
-    {
-        Console.WriteLine("Cvrenin sahesi:" + Area(2));
-        Console.WriteLine("Duzbucaqlinin sahesi:" + Area(2, 3));
-        Console.WriteLine("Duzbucaqli paralelpipedin sahesi:" + Area(2, 3, 4));
-        Console.WriteLine("3bucagin daxiline cekilmis cevrenin sahesi:" + Area(2, 3, 4, 5));
-    }
+//    {
+//        Console.WriteLine("Cvrenin sahesi:" + Area(2));
+//        Console.WriteLine("Duzbucaqlinin sahesi:" + Area(2, 3));
+//        Console.WriteLine("Duzbucaqli paralelpipedin sahesi:" + Area(2, 3, 4));
+//        Console.WriteLine("3bucagin daxiline cekilmis cevrenin sahesi:" + Area(2, 3, 4, 5));
+//    }
 
-}
+//}
 
 
 
